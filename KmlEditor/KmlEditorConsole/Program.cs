9d51d25ef7e5d4567953014c62ba9cfeff47d7e2
panglat <panglat@gotocme.com>
2016-07-12 23:18:50 -0300

    First commit

---
+++ KmlEditor/KmlEditorConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using KmlEditorLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KmlEditorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String inFile = @"C:\tmp\Test\todalared.kmz";
+            String outFile = @"C:\tmp\Test\out.kml";
+            KmlEditor kmlEditor = new KmlEditor();
+            kmlEditor.openFile(inFile);
+        }
+    }
+}
---
+++ KmlEditor/KmlEditorLibrary/KmlEditor.cs
@@ -0,0 +1,36 @@
+﻿using SharpKml.Engine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KmlEditorLibrary
+{
+    public class KmlEditor
+    {
+        KmlFile kmlFile = null;
+
+        public void openFile(String filePath)
+        {
+            string fileExtension = System.IO.Path.GetExtension(filePath);
+            using (FileStream fileStream = File.OpenRead(filePath))
+            {
+                if (fileExtension.Equals(".kmz", StringComparison.OrdinalIgnoreCase))
+                {
+                    KmzFile kmzFile = KmzFile.Open(fileStream);
+                    string kmlFileString = kmzFile.ReadKml();
+                    using (StringReader stringReader = new StringReader(kmlFileString))
+                    {
+                        kmlFile = KmlFile.Load(stringReader);
+                    }
+                }
+                else
+                {
+                    kmlFile = KmlFile.Load(fileStream);
+                }
+            }
+        }
+    }
+}
